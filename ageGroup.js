fc59ac1316d2adb1d113471c9d58cda1543328ab
AYUSHA SINGH <39898651+feak-fuzz@users.noreply.github.com>
2020-04-13 22:29:30 +0530

    Update ageGroup.js

--- ageGroup.js
+++ ageGroup.js
@@ -37,6 +37,6 @@ window.onload = function() {
 	 
 	}
 	 
-	$.getJSON(https://covid19-ayusha.herokuapp.com/api/AgeGroupDetails", addData);
+	$.getJSON("https://covid19-ayusha.herokuapp.com/api/AgeGroupDetails", addData);
 	 
 	}
